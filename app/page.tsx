8423e9bff8ea6df178f1d761d0e23712182e888b
yewonkk11 <yewon@sagolink.com>
2026-04-06 14:06:10 +0900

    style: global css 적용

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const test = { a: 1, b: 2 };
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="bg-red-500 text-white p-4">테스트</div>
+      <div className="bg-red-500 text-white p-4 font-40-extrabold">테스트</div>
     </div>
   );
 }
